66abda062b7d4fd6ad91832aafc4cbb59506c6ae
660610809-kornkawin <kornkawin_th2cmu.ac.th>
2023-12-15 16:57:15 +0700

    lab9-2-completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N, int M){
+	if (N>0 && M>0){
+		for(int i =0; i<N; i++){
+			for (int i=0; i<M; i++){
+				cout<< "O";
+			}cout<<"\n";
+		}
+		}else {
+			cout << "Invalid input";
+		}
+	}
+	
 
 int main(){
 	
